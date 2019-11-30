c97f02981c6713e608d1252c3d42745799f8fd9c
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-11-30 09:32:39 -0600

    updated java

--- js/javascript.js
+++ js/javascript.js
@@ -6,13 +6,21 @@ function animatedForm() {
        const input = arrow.previousElementSibling;
        const parent = arrow.parentElement;
        const nextForm = parent.nextElementSibling;
+
+       if(input.type === "text" && validateUser(input)) {
+           console.log('it works')
+       }
         });
     });
 }
 
 function validateUser(user) {
     if(user.value.length < 6) {
-        console.log('not enough characters')
+       console.log('not enough characters')
+        error('rgb(,189, 87, 87');
+    } else {
+        error('rgb(87, 189, 130');
+        return true;
     }
 }
 
