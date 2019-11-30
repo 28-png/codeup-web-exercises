8a75d362b0748de98c07ff9b2b340c2e9823f5b1
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-11-30 00:21:08 -0600

    updated java

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+function animatedForm() {
+    const arrows = document.querySelectorAll(".fa-arrow-down");
+
+    arrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+       const input = arrow.previousElementSibling;
+       const parent = arrow.parentElement;
+       const nextForm = parent.nextElementSibling;
+        });
+    });
+}
+
+function validateUser(user) {
+    if(user.value.length < 6) {
+        console.log('not enough characters')
+    }
+}
+
+function error(color) {
+    document.body.backgroundColor = color;
+}
+
+animatedForm();
