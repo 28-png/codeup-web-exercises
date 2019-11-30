9f5c3745989164aca9a53c76d7a6d11773ba6256
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-11-30 10:10:57 -0600

    updated java

--- js/javascript.js
+++ js/javascript.js
@@ -8,7 +8,9 @@ function animatedForm() {
        const nextForm = parent.nextElementSibling;
 
        if(input.type === "text" && validateUser(input)) {
-           console.log('it works')
+           nextSlide(parent, nextForm);
+      } else if (input.type === "email" && validateEmail(input)) {
+           nextSlide(parent, nextForm);
        }
         });
     });
@@ -16,16 +18,32 @@ function animatedForm() {
 
 function validateUser(user) {
     if(user.value.length < 6) {
+        error('rgb(189, 87, 87)');
        console.log('not enough characters')
-        error('rgb(,189, 87, 87');
     } else {
-        error('rgb(87, 189, 130');
+        error('rgb(87, 189, 130)');
         return true;
     }
 }
 
+function validateEmail(email) {
+    const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(validation.test(email.value)) {
+      error('rgb(87, 189, 130)');
+        return true;
+    } else {
+        error('rgb(189, 87, 87)');
+    }
+}
+
+function nextSlide(parent, nextForm) {
+    parent.classList.add('inactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active');
+}
+
 function error(color) {
-    document.body.backgroundColor = color;
+    document.body.style.backgroundColor = color;
 }
 
 animatedForm();
