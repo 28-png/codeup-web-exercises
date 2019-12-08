373f869e74fac06c9c7dd15bd2964e13cc93723c
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-08 12:15:19 -0600

    updated exercise

--- js/while.js
+++ js/while.js
@@ -4,10 +4,17 @@
 //     console.log(i);
 // }
 
+var allCones = Math.floor(Math.random() * 50) + 50;
+
 do {
-    // This is how you get a random number between 50 and 100
-    var allCones = Math.floor(Math.random() * 50) + 50;
-// This expression will generate a random number between 1 and 5
-     var oneThroughFive = Math.floor(Math.random() * 5) + 1;
-     
-} while()
+    var cones = Math.floor(Math.random() * 5) + 1;
+    allCones = allCones - cones;
+    console.log("I sold " + cones + " cones.");
+    if (cones > allCones)
+    {console.log("I cannot sell you " + cones + ", I only have " + allCones + " left.")
+    }
+
+}
+
+while (allCones > 0);
+console.log("Yay! I sold them all!")
