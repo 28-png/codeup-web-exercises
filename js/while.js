4ed5bf47c8e43e3eb26ac48989ce51f09dec2c92
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-08 14:16:15 -0600

    updated loops exercise

--- js/while.js
+++ js/while.js
@@ -4,17 +4,18 @@
 //     console.log(i);
 // }
 
-var allCones = Math.floor(Math.random() * 50) + 50;
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// do {
+//     var cones = Math.floor(Math.random() * 5) + 1;
+//     allCones = allCones - cones;
+//     console.log("I sold " + cones + " cones.");
+//     if (cones > allCones)
+//     {console.log("I cannot sell you " + cones + ", I only have " + allCones + " left.")
+//     }
+//
+// }
+//
+// while (allCones > 0);
+// console.log("Yay! I sold them all!")
 
-do {
-    var cones = Math.floor(Math.random() * 5) + 1;
-    allCones = allCones - cones;
-    console.log("I sold " + cones + " cones.");
-    if (cones > allCones)
-    {console.log("I cannot sell you " + cones + ", I only have " + allCones + " left.")
-    }
-
-}
-
-while (allCones > 0);
-console.log("Yay! I sold them all!")
