cd13b3d51ea16dfdb287afa9de3a61184c5d2825
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-09 12:30:33 -0600

    completed exercises

--- js/break-and-continue.js
+++ js/break-and-continue.js
@@ -1,10 +1,11 @@
+"use strict"
 // var random = Math.floor((Math.random()*50)+1);
 //
 // console.log("Random number to skip is: " + random);
 //
-// for (var i = 0; i < 50; i++) {
+// for (var i = 1; i < 50; i++) {
 //     if (i % 2 === 0) {
-//         continue;
+//       continue;
 //
 //     }
 //
@@ -15,8 +16,24 @@
 //     }
 //
 //     if (i >= 50) {
-//         break;
+//        break;
 //
 //     }
+//
+// }
+//alternate solution
+// do {
+//     var userNumber = Number(prompt("Pick an odd number between 1 and 50"));
+//     if (userNumber % 2 === 0)
+//         alert(userNumber + " is not odd, please pick again")
+//     else if (userNumber < 1 || userNumber > 50)
+//         alert(userNumber + " is not between 1 and 50, please pick again");
+//     else if (isNaN(userNumber))
+//         alert(userNumber + " is not a number, please pick again");
+//     else {
+//         alert("congrats you picked the right number");
+//         break;
+//     }
+// } while (true) {
 //
 // }
