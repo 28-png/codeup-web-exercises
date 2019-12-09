63ae699385998b0d19f38027f7c317e8f4867990
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-09 06:54:44 -0600

    updated exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,22 @@
-for(var i = 1; i <= 10; i++) {
-var timesSeven = i * 7;
-console.log("7 x " + i + " = " + timesSeven);
+// for(var i = 1; i <= 10; i++) {
+// var timesSeven = i * 7;
+// console.log("7 x " + i + " = " + timesSeven);
+//
+// }
 
-}
+// var randomNumbers = Math.floor(Math.random() * (200 - 20)) + 20;
+// for(var i = 1; i <= 10; i++) {
+// var odd = randomNumbers % 2;
+// var even = randomNumbers % 2 === 0;
+// console.log(i + randomNumbers + even + " is even")
+//     console.log(i + randomNumbers + odd + " is odd")
+//
+// }
+
+
+
+
+
+
+
 
