ba7777edd339ed0d8381bff2c8505f5b359a6bc1
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-10 11:13:32 -0600

    completed arrays lec

--- js/arrays-lec.js
+++ js/arrays-lec.js
@@ -1,22 +1,47 @@
-var pets = [];
-pets[0] = "Toby";
-pets[1] = "Princess";
-pets[2] = "Samson";
-pets[3] = "Jill";
-pets[4] = "Bubbles";
-pets[5] = "Malu";
+// var pets = [];
+// pets[0] = "Toby";
+// pets[1] = "Princess";
+// pets[2] = "Samson";
+// pets[3] = "Jill";
+// pets[4] = "Bubbles";
+// pets[5] = "Malu";
+//
+// console.log(pets)
+//
+// for (var i = 0; i < pets.length; i++) {
+//
+// }
+//
+// pets.forEach(function (pet, i, pets) {
+//
+//     console.log(pet);
+//
+// })
 
-console.log(pets)
+// var sentence = "The quick brown fox jumps over the lazy dog";
+// console.log(sentence);
+// var words = sentence.split(" ");
+// for (var i = 0;i<words.length; i++) {
+//     // capitalize first letter
+//     var word = words[i];
+//     // var firstLetter = word.split()[0];
+//     var letters = word.split("");
+//     var capitalFirstLetter = letters[0].toUpperCase();
+//     var capitalizedWord = "";
+//     capitalizedWord += capitalFirstLetter;
+//     for (var j=1;j<letters.length;j++) {
+//         capitalizedWord += letters[j];
+//     }
+//     words[i] = capitalizedWord;
+// }
+// sentence = words.join(" ");
+// console.log(sentence);
 
-for (var i = 0; i < pets.length; i++) {
 
-}
 
-pets.forEach(function (pet, i, pets) {
 
-    console.log(pet);
 
-})
+
 
 
 
