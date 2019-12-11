0d94405dc5bb727e52804985da3d3eecc43fb74a
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-11 09:02:19 -0600

    updated

--- js/objects.js
+++ js/objects.js
@@ -44,12 +44,20 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
 
+    shoppers.moreThan = function () {
+        if(shoppers.amount > 200) {
+            return (person.amount * .12) + person.amount;
+        } else {
+            return "no discount"
+        }
+    }
+    console.log(shoppers.moreThan());
     /** TODO:
      * Create an array of objects that represent books and store it in a
      * variable named `books`. Each object should have a title and an author
