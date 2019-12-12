acc5bf29a9e3fcc117ea3b8f16061d37abd9fcad
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-12 09:34:50 -0600

    completed math exercise

--- js/circle.js
+++ js/circle.js
@@ -35,10 +35,33 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-    
+    var circle = {
+        radius: 5,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+            var area = Math.PI * Math.pow(this.radius, 2);
+            return area; // TODO: return the proper value
+
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            if(doRounding === true) {
+                return Math.round(this.getArea());
+            } else if(doRounding === false) {
+                return this.getArea()
+            }
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea());
+        }
+    };
     // log info about the circle
     console.log("Raw circle information");
-    circle.logInfo(false);
+    console.log(circle.logInfo(false));
     console.log("Circle information rounded to the nearest whole number");
-    circle.logInfo(true);
+    console.log(circle.logInfo(true));
 })();
