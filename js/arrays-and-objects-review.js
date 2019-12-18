4b6e2b1cf9bf5d27664257eff1aa6f5f3b486d91
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-18 07:59:21 -0600

    added arrays and objects review

---
+++ js/arrays-and-objects-review.js
@@ -0,0 +1,31 @@
+// 1. What is an array in Javascript?
+//
+//     2. What is an object in Javascript?
+//
+//     3. What is the difference between an array and an object?
+//
+//     4. What is a property? A method?
+//
+//     5. Why are loops useful when dealing with arrays?
+//
+//     6. What is an index?
+//
+//     7. What is the difference between dot and bracket notation?
+
+
+// 1.  Write a function, `filterNumbers()` that takes in an array of mixed data types and returns an array of only the numbers type in ascending order.
+//
+//     ```js
+//         filterNumbers(["fred", true, 5, 3]) //[3, 5]
+
+
+
+
+
+
+
+
+
+
+
+
