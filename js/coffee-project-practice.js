7aa6afaf2232bbb86a5cbc7f76987d914f0c5c6d
Matt Murphy <murphy.d.matthew28@gmail.com>
2019-12-24 13:12:06 -0700

    updated

--- js/coffee-project-practice.js
+++ js/coffee-project-practice.js
@@ -25,7 +25,7 @@ function updateCoffees(e) {
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
-        } else if(coffee.allRoast === selectedRoast)
+        } else if(coffee.roast === )
             filteredCoffees.push(coffee);
     });
     pList.innerHTML = renderCoffees(filteredCoffees);
@@ -46,20 +46,20 @@ function search_coffee() {
 }
 
 var coffees = [
-    {id: 1, name: 'National Treasure 2 Light City', roast: 'light', allRoast: "all roast" },
-    {id: 2, name: 'National Treasure Half City', roast: 'light', allRoast: "all roast" },
-    {id: 3, name: 'Con-Air Cinnamon', roast: 'light', allRoast: "all roast" },
-    {id: 4, name: 'Drive Angry City', roast: 'medium', allRoast: "all roast" },
-    {id: 5, name: 'Gone in Sixty Seconds American', roast: 'medium', allRoast: "all roast" },
-    {id: 6, name: 'Face-Off Breakfast', roast: 'medium', allRoast: "all roast" },
-    {id: 7, name: 'Matchstick Men High', roast: 'dark', allRoast: "all roast" },
-    {id: 8, name: 'The Wicker Man Continental', roast: 'dark', allRoast: "all roast" },
-    {id: 9, name: 'Lord Of War New Orleans', roast: 'dark', allRoast: "all roast" },
-    {id: 10, name: 'The European Ant Bully', roast: 'dark', allRoast: "all roast" },
-    {id: 11, name: 'World Trade Center Espresso', roast: 'dark', allRoast: "all roast" },
-    {id: 12, name: 'Bangkok Dangerous Viennese', roast: 'dark', allRoast: "all roast" },
-    {id: 13, name: 'Grindhouse Italian', roast: 'dark', allRoast: "all roast" },
-    {id: 14, name: 'The French Family Man', roast: 'dark', allRoast: "all roast"},
+    {id: 1, name: 'National Treasure 2 Light City', roast: 'light' },
+    {id: 2, name: 'National Treasure Half City', roast: 'light' },
+    {id: 3, name: 'Con-Air Cinnamon', roast: 'light' },
+    {id: 4, name: 'Drive Angry City', roast: 'medium' },
+    {id: 5, name: 'Gone in Sixty Seconds American', roast: 'medium' },
+    {id: 6, name: 'Face-Off Breakfast', roast: 'medium' },
+    {id: 7, name: 'Matchstick Men High', roast: 'dark' },
+    {id: 8, name: 'The Wicker Man Continental', roast: 'dark' },
+    {id: 9, name: 'Lord Of War New Orleans', roast: 'dark' },
+    {id: 10, name: 'The European Ant Bully', roast: 'dark' },
+    {id: 11, name: 'World Trade Center Espresso', roast: 'dark' },
+    {id: 12, name: 'Bangkok Dangerous Viennese', roast: 'dark' },
+    {id: 13, name: 'Grindhouse Italian', roast: 'dark' },
+    {id: 14, name: 'The French Family Man', roast: 'dark'},
 ];
 
 
