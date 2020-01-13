44d811b7fbdcfc4579d5d91c1ea20028295fcd59
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-13 07:41:42 -0600

    updated

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -44,10 +44,10 @@ function reverseGeocode(coordinates, token) {
     var endPoint = '/geocoding/v5/mapbox.places/';
     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
         .then(function(res) {
-            return res.json();
+           return res.json();
         })
         // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
-            return data.features[0].place_name;
-        });
+        // .then(function(data) {
+        //     return data.features[0].place_name;
+        // });
 }
