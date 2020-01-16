6beb33668549ec31fc517b7a141ee5226d9eaeb1
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-16 15:11:16 -0600

    updat4ed

--- js/es6.js
+++ js/es6.js
@@ -33,31 +33,32 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'Matt';
+const email = 'murphy.d.matthew28@gmail.com';
+const languages = ['html', 'css', 'javascript', 'jQuery'];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
+users.forEach(user => {
   return emails.push(user.email);
 });
-users.forEach(function(user) {
+
+users.forEach(user  => {
   return names.push(user.name);
 });
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
