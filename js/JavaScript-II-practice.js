c7acd48e9723c25f6e779ee1d87672c283c86dd7
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-16 14:58:37 -0600

    updated

--- js/JavaScript-II-practice.js
+++ js/JavaScript-II-practice.js
@@ -22,7 +22,8 @@ explore();
 
 let dog = {
     name:"dog",
-    breed:"Chihuahua"
+    breed:"Chihuahua",
+    age: 2
 }
 
 document.getElementById('message').innerHTML = `<p>Our ${dog.breed} is named ${dog.name}</p>`;
@@ -39,11 +40,26 @@ const add7 = num => num + 7;
 
 const newHello = (name2 = "Codeup") => `Hello, ${name2}!`; // first is default parameter then parameter when name is passed through
 console.log(newHello("europa"));
-    console.log(newHello())
+    console.log(newHello());
 
-    const sayHello = (myName = "name") => `Hello, ${myName}!`
-    console.log(sayHello("Matt"))
-    console.log(sayHello())
+    const sayHello = (myName = "name") => `Hello, ${myName}!`;
+    console.log(sayHello("Matt"));
+    console.log(sayHello());
+
+    const friend = {
+        name: "Calvin",
+        age: 7
+    };
+
+    const tellMeAbout = ({name = "name",age = 0}) => {
+        return `${name} is awesome!
+        ${name} is ${age} years old.`
+    }
+
+    console.log(tellMeAbout(friend))
+    console.log(tellMeAbout(dog))
+
+
 
 
 }
