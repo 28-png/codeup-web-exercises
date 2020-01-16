0f12405f082a2a681830017c304edab2df12d871
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-16 13:33:23 -0600

    added javascript II practice file

---
+++ js/JavaScript-II-practice.js
@@ -0,0 +1,35 @@
+{ // New IFFE
+    "use strict"
+
+// old way
+console.log(Math.pow(2, 8)); // === 256
+// new syntax
+
+for(let i = 0; i < 16; i++) {
+
+console.log(2 ** i); // === 256
+
+}
+
+function explore() {
+    let a = 1;
+    let b = 2;
+    console.log(a);
+    console.log(b);
+}
+
+explore();
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
