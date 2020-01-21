648c1b7a64de94fff06cb3c7728fd1166238d54f
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-21 12:02:00 -0600

    updated

--- js/promises.js
+++ js/promises.js
@@ -1,42 +1,33 @@
 {
-   "use strict"
+    "use strict";
 
-    function wait(seconds) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (Math.random() > 0.1) {
-               resolve('Here is your data: ...');
-            } else {
-                reject('Network Connection Error!');
-            }
-        }, seconds);
-    });
-}
-
-    wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-    wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
-
-
+//     function wait(seconds) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (Math.random() > 0.1) {
+//                resolve('Here is your data: ...');
+//             } else {
+//                 reject('Network Connection Error!');
+//             }
+//         }, seconds);
+//     });
+// }
+//
+//     wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+//     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
+    function getGithubUsernames(username) {
+        return fetch(`https://api.github.com/search/commits?q=sort:comitter+date:${username}`,
+            {headers: {'Authorization': github, 'Accept': 'application/vnd.github.cloak-preview', }})
+            .then(response => response.json())
+            .then(res => {
+                console.log(res)
+            });
+    }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(getGithubUsernames("28-png"))
 
 
 }
