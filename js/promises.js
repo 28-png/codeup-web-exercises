a83eaf7b603b21cf241ce08b4c88016d3197fe91
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-01-21 14:54:06 -0600

    updated

--- js/promises.js
+++ js/promises.js
@@ -1,20 +1,20 @@
 {
     "use strict";
 
-    function wait(seconds) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (Math.random() > 0.1) {
-               resolve('Here is your data: ...');
-            } else {
-                reject('Network Connection Error!');
-            }
-        }, seconds);
-    });
-}
-
-    wait(1000).then(() => console.log('You\'ll see this after 1 second'));
-    wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+//     function wait(seconds) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (Math.random() > 0.1) {
+//                resolve('Here is your data: ...');
+//             } else {
+//                 reject('Network Connection Error!');
+//             }
+//         }, seconds);
+//     });
+// }
+//
+//     wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+//     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
     function getGithubUsernames(username) {
         return fetch(`https://api.github.com/search/commits?q=sort:committer-date+committer:${username}`,
